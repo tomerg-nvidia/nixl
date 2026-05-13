60ba495a71d2bcc73fa8c19435620cdfced68572
Tomer Gilad <tgilad@nvidia.com>
2026-05-13 15:31:36 +0200

    PLUGINS/UCX: set MAX_HCA_PER_GPU to auto

--- src/plugins/ucx/ucx_utils.cpp
+++ src/plugins/ucx/ucx_utils.cpp
@@ -427,6 +427,10 @@ nixlUcxContext::nixlUcxContext(const std::vector<std::string> &devs,
         config.modify("RC_GDA_NUM_CHANNELS", std::to_string(num_device_channels));
     }
 
+    if (ucpVersion_ >= UCP_VERSION(1, 21)) {
+        config.modify("MAX_HCA_PER_GPU", "auto");
+    }
+
     const auto &hw_info = nixl::hwInfo::instance();
     if (hw_info.numEfaDevices != 0) {
         config.modify("ADDRESS_VERSION", "v2");
